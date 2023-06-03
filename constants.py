019b903b826d1d706a2a9ea91f94a1bec1877d13
williambrach <wibrach@gmail.com>
2023-06-03 14:30:10 +0200

    init commit

---
+++ constants.py
@@ -0,0 +1,40 @@
+import logging
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+
+def get_champ_name_to_id() -> dict:
+    n2id = {}
+    champs_assets = requests.get(
+        "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/en_gb/v1/champion-summary.json"
+    ).json()
+    for c in champs_assets:
+        n2id[c["name"]] = c["id"]
+
+    new_n2id = {}
+    for k in n2id:
+        if " " in k:
+            new_k = k.replace(" ", "")
+            new_n2id[new_k] = n2id[k]
+    n2id.update(new_n2id)
+    return n2id
+
+
+def create_logger() -> logging.getLogger:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
+
+
+n2id = get_champ_name_to_id()
+LOL_API_KEY = os.getenv("LOL_API_KEY")
+logger = create_logger()
---
+++ main.py
@@ -0,0 +1,9 @@
+from constants import logger
+
+
+def main() -> None:
+    logger.info("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
